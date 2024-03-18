ba92c633b7abaf08de0c542d8da4cd3dae279bc4
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-18 13:52:31 +0545

    setup: basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import RoutesContainer from "./routes";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-red-600">Hwllo world</h1>
-    </>
-  );
-}
-
+const App = () => {
+  return <RoutesContainer />;
+};
 export default App;
---
+++ src/components/navbar/Sidebar.jsx
@@ -0,0 +1,4 @@
+const Sidebar = () => {
+  return <aside className=" w-[242px] border-r-black bg-slate-500">Sidebar</aside>;
+};
+export default Sidebar;
