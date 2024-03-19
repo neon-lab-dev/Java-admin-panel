70049f6a6c3ddb3252fd43d8b268c9575fa9a563
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-03-19 23:54:30 +0530

    fea:users

---
+++ src/components/Searchbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SeachIcon from "../../assets/icon/searchIcon.svg"
+
+
+const Searchbar = ({ placeholder }) => {
+    return (
+        <div className="flex  border-[0.6px] border-[#D5D5D5] bg-slate-100 h-[38px] rounded-full  items-center gap-2 ps-3 sm:max-w-[388px] md:w-[388px]">
+            <img className="h-[15.01px] w-[15.01px]" src={SeachIcon} alt="" />
+            <input className="w-full me-3 border-none focus:outline-none font-lato text-[14px] font-medium bg-slate-100  " placeholder={placeholder} type="text" />
+        </div>
+
+    )
+}
+
+export default Searchbar
