f13368cfba33bec2b09e103648ca682ae7d199fb
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-03-20 22:47:23 +0530

    fix:gap of dashboard

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return ( 
   <div className="text-primary bg-[#F5F6FA] min-h-svh w-full px-6 py-2">
     <div className="text-neutral-800 text-[32px] font-bold font-['Lato']">Java Sports Admin Dashboard</div>
-    <div className="flex flex-wrap gap-x-14 place-items-center gap-y-4 py-[33px] ">
+    <div className="flex flex-wrap gap-14 py-[33px] ">
       {dashboardData.map((element)=>{
         return(
           <div key={element.title}>
