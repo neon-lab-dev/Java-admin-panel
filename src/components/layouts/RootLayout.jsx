1a21ea8ac90dedfa582c361f1611ea7ef2dd299f
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-03-25 21:19:20 +0530

    fix:Error and hooks

--- src/components/layouts/RootLayout.jsx
+++ src/components/layouts/RootLayout.jsx
@@ -1,14 +1,11 @@
 import Sidebar from "../navbar/Sidebar";
 import Header from "../navbar/Header";
-import { useInnerSize } from "../Hooks/InnerSizeHook";
+import { useInnerSize } from "../../hooks/InnerSizeHook";
 import Error from "../../pages/Error";
-import { useNavigate } from "react-router-dom";
 
 const RootLayout = ({ children }) => {
   let size = useInnerSize();
-  let navigate = useNavigate();
   if(size.width < 700 || size.height<500) {
-    navigate("*")
     return (
       <Error/>
     )
