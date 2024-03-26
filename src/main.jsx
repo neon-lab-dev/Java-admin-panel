23afb66c0525519f4005f41f0f9eb8d391e4c980
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-26 21:44:16 +0545

    Add React Query Devtools

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "./store/index.js";
 import { Provider as ReduxProvider } from "react-redux";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
       <ReduxProvider store={store}>
         <App />
+        {import.meta.env.DEV && <ReactQueryDevtools  />}
       </ReduxProvider>
     </QueryClientProvider>
   </React.StrictMode>
