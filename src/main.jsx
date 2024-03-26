456145bb5d2d8860396917f5cfb7cd5a3446bc6f
Tilak Thapa <iamjrtilak@gmail.com>
2024-03-26 11:05:25 +0545

    feat: store setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import store from "./store/index.js";
+import { Provider as ReduxProvider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
