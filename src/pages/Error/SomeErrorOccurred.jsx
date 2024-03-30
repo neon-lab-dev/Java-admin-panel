53dcd68723a6e68836bf8d73eefdd06f47ae78f4
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-03-30 17:17:17 +0530

    fea:someErrorOccurred

--- src/pages/Error/SomeErrorOccurred.jsx
+++ src/pages/Error/SomeErrorOccurred.jsx
@@ -1,6 +1,13 @@
+import {useNavigate } from "react-router-dom"
+
 const SomeErrorOccurred = () => {
+  const navigate = useNavigate();
   return (
-    <div>SomeErrorOccurred</div>
+    <div className="flex h-screen flex-col justify-center items-center">
+      <div className="text-9xl ">404</div>
+      <div className="mt-7 text-2xl">The page you are looking for was not found</div>
+      <button className="text-blue-500 mt-3" onClick={()=>{navigate("/")}} > Back to Home</button>
+    </div>
   )
 }
 export default SomeErrorOccurred
