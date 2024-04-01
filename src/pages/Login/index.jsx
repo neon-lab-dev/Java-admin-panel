796a605b4c674dcb14ee84ee5aebf361bd4ef3b2
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-01 08:21:12 +0545

    Add password visibility toggle

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/icon/logo.svg";
 import { useForm } from "react-hook-form";
 import { EMAIL_REGEX } from "../../assets/data/regex";
@@ -9,8 +9,11 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Navigate, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import { setIsAuthenticating } from "../../store/slices/userSlice";
+import eye from "../../assets/icons/eye-svgrepo-com.svg";
+import eyeSlash from "../../assets/icons/eye-slash-svgrepo-com.svg";
 
 const Login = () => {
+  const [passwordType, setPasswordType] = useState("password");
   const dispatch = useDispatch();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
@@ -32,7 +35,6 @@ const Login = () => {
         title: "Oops...",
         text: error,
       });
-      reset();
     },
     onSuccess: () => {
       dispatch(setIsAuthenticating(true));
@@ -51,11 +53,11 @@ const Login = () => {
     return <Navigate to="/" />;
   }
   return (
-    <div className="flex flex-col place-content-center place-items-center justify-center items-center h-screen">
-      <div className="lg:w-[888px] lg:h-[672px] w-4/6 h-5/6 min-h-[510px] lg:min-h-[712px] bg-white rounded-[20px] border border-stone-300  ">
+    <div className="flex justify-center items-center h-screen w-screen p-24 bg-white">
+      <div className="rounded-[20px] border border-stone-300 py-6 xl:py-10 px-24">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="w-full h-full flex flex-col lg:place-content-center place-items-center items-center mt-[29px]"
+          className="h-full flex flex-col items-center justify-center w-[500px]"
         >
           {/* {image and Dashboard} */}
           <div className="flex flex-col w-full justify-center items-center place-content-center place-items-center">
@@ -65,41 +67,37 @@ const Login = () => {
                 src={logo}
               />
             </div>
-            <p className="flex w-full justify-center  text-zinc-800 lg:text-[32px] text-[18px] font-medium font-Lato leading-loose mt-[18px]">
+            <p className="flex w-full justify-center text-zinc-800 lg:text-[32px] text-[18px] font-medium font-Lato leading-loose mt-[18px]">
               Dashboard Login
             </p>
           </div>
           {/* Email */}
-          <div className="lg:mt-[36px] mt-[20px] w-full flex flex-col items-center">
-            <div className="flex flex-col items-center w-5/6">
-              <p className="lg:w-[515px] w-5/6  text-zinc-800  text-start text-base font-bold font-Lato leading-tight">
-                Email
-              </p>
-            </div>
-            <div className="flex flex-col items-center w-5/6">
-              <input
-                className="mt-2 lg:w-[515px] lg:h-[54px] w-5/6 bg-white rounded-[10px] border border-stone-300 placeholder:opacity-40 leading-9 pl-[16px] text-base font-light font-Lato"
-                type="email"
-                placeholder="Enter Your Email"
-                {...register("email", {
-                  validate: (value) =>
-                    EMAIL_REGEX.test(value) || "Invalid email",
-                })}
+          <div className="w-full flex flex-col gap-2">
+            <p className="text-zinc-800  text-start text-base font-bold font-Lato leading-tight w-full">
+              Email
+            </p>
+            <input
+              className="w-full h-[50px] lg:h-[54px] bg-white rounded-[10px] border border-stone-300 placeholder:opacity-40 leading-9 pl-[16px] text-base font-light font-Lato"
+              type="email"
+              placeholder="Enter Your Email"
+              {...register("email", {
+                validate: (value) => EMAIL_REGEX.test(value) || "Invalid email",
+              })}
+            />
+            {errors.email && (
+              <AppFormErrorLine
+                message={errors.email.message}
+                className="self-start"
               />
-              {errors.email && (
-                <AppFormErrorLine message={errors.email.message} />
-              )}
-            </div>
+            )}
             {/* password */}
-            <div className="flex flex-col mt-[24px] items-center w-5/6">
-              <p className="lg:w-[515px] w-5/6  text-zinc-800 text-base font-bold font-Lato leading-tight">
-                Password
-              </p>
-            </div>
-            <div className="flex flex-col items-center w-5/6">
+            <p className=" mt-4 text-zinc-800 text-base font-bold font-Lato leading-tight">
+              Password
+            </p>
+            <div className="relative w-full">
               <input
-                className="mt-2 lg:w-[515px] lg:h-[54px] w-5/6 bg-white rounded-[10px] border border-stone-300 placeholder:opacity-40 leading-9 pl-[16px] text-base font-light font-Lato"
-                type="password"
+                className="w-full h-[50px] lg:h-[54px] bg-white rounded-[10px] border border-stone-300 placeholder:opacity-40 leading-9 pl-[16px] text-base font-light font-Lato"
+                type={passwordType}
                 placeholder="Enter Your Password"
                 {...register("password", {
                   required: "Password is required",
@@ -109,15 +107,36 @@ const Login = () => {
                   },
                 })}
               />
-              {errors.password && (
-                <AppFormErrorLine message={errors.password.message} />
-              )}
+              <button
+                role="button"
+                type="button"
+                className="absolute right-4 top-1/2 -translate-y-1/2"
+                onClick={() => {
+                  if (passwordType === "password") {
+                    setPasswordType("text");
+                  } else {
+                    setPasswordType("password");
+                  }
+                }}
+              >
+                <img
+                  src={passwordType === "password" ? eye : eyeSlash}
+                  alt="eye"
+                  className="w-5 h-5"
+                />
+              </button>
             </div>
+            {errors.password && (
+              <AppFormErrorLine
+                message={errors.password.message}
+                className="self-start"
+              />
+            )}
           </div>
           <button
             type="submit"
             disabled={Object.keys(errors).length > 0}
-            className="lg:mt-[36px] mt-[20px] lg:w-[515px] lg:h-16 w-4/6 h-10 bg-rose-500 rounded-xl disabled:opacity-60 disabled:cursor-default"
+            className="w-full mt-6 lg:h-16 h-14 bg-rose-500 rounded-xl disabled:opacity-60 disabled:cursor-default"
           >
             {isPending ? (
               <span className="loading loading-spinner loading-md text-white" />
