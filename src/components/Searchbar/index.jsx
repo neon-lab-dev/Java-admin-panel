5a9b4f05e89306e2627892c6c328b3e9f0dddf33
ayush050501 <ayushagrawal133.skb@gmail.com>
2024-04-01 21:27:14 +0530

    fix:JAP2 and 3

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -2,7 +2,7 @@ import SearchIcon from "../../assets/icons/searchIcon.svg";
 
 const Searchbar = ({ ...props }) => {
   return (
-    <div className="flex  border-[0.6px] border-[#D5D5D5] bg-lightgray h-[38px] rounded-full  items-center gap-2 ps-3 sm:max-w-[388px] md:w-[388px]">
+    <div className="flex w-full mr-20 border-[0.6px] border-[#D5D5D5] bg-lightgray h-[38px] rounded-full  items-center gap-2 ps-3 md:w-[388px]">
       <button className="h-[15.01px] w-[15.01px] ">
         <img src={SearchIcon} alt="" />
       </button>
